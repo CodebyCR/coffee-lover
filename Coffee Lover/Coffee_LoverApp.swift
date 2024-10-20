0789eb123079753244eb75a54aea06303c97da21
Christoph <christoph_rohde@icloud.com>
2024-10-20 10:36:05 +0200

    Initial Commit

---
+++ Coffee Lover/Coffee_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_LoverApp.swift
+//  Coffee Lover
+//
+//  Created by Christoph Rohde on 20.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee LoverTests/Coffee_LoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_LoverTests.swift
+//  Coffee LoverTests
+//
+//  Created by Christoph Rohde on 20.10.24.
+//
+
+import Testing
+@testable import Coffee_Lover
+
+struct Coffee_LoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
