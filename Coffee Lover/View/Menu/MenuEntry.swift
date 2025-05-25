4dc14fed6f7e8c75407d60b2d8d13bb701218e30
Christoph <christoph_rohde@icloud.com>
2025-05-25 19:02:44 +0200

    Extreact ProductImageMinView

--- Coffee Lover/View/Menu/MenuEntry.swift
+++ Coffee Lover/View/Menu/MenuEntry.swift
@@ -9,11 +9,16 @@ import Coffee_Kit
 import SwiftUI
 
 struct MenuEntry: View {
+    @State
     var productEntry: Product
 
     var body: some View {
         HStack {
+
             HStack {
+
+                ProductImageMinView(product: $productEntry)
+
                 Text("\(productEntry.categoryNumber)")
                     .fontWeight(.thin)
                     .italic()
@@ -24,13 +29,51 @@ struct MenuEntry: View {
                     .italic()
             }
 
+
+
             Spacer()
 
             Text(CurrencyFormatter.formatAmount(productEntry.price))
                 .italic()
-        }.frame(
-            height: 60
-        )
-        .padding(.leading, 8)
+
+            }.frame(height: 60)
+//            .padding(.leading, 8)
+        }
+
+}
+
+
+#Preview {
+    MenuEntry(productEntry: Product())
+    .preferredColorScheme(.dark)
+}
+
+struct ProductImageMinView: View {
+    @Binding var product: Product
+    let framesize: CGFloat = 54
+
+    var body: some View {
+        VStack {
+            if let imageURL = product.imageUrl {
+                AsyncImage(url: imageURL) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: framesize, height: framesize)
+                        .cornerRadius(8)
+                        .padding()
+                    
+                } placeholder: {
+                    ProgressView()
+                        .frame(width: framesize, height: framesize)
+                }
+            } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: framesize, height: framesize)
+                    .padding()
+            }
+        }
     }
 }
